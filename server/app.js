f97679b0460176d542f483fac6d10f40bd11b0ae
Habeetech <oluhabnoni@gmail.com>
2026-02-24 08:55:34 +0000

    initial commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, you've hit the server root")
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
