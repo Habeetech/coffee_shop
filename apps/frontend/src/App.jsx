f4792b9c5e0ab435ca47a7ae2a177919b99947cb
Habeetech <oluhabnoni@gmail.com>
2026-03-01 21:53:33 +0000

    Feat: created a menupage and injected menu component

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 
 import './App.css'
-import Home from "./pages/Home.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import MenuPage from "./pages/MenuPage.jsx";
 import MainLayout from "../src/layouts/MainLayout.jsx"
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
@@ -10,7 +11,8 @@ function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
       </Route>
     </Routes>
   )
