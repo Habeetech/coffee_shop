90cd22640e17ed388f24825d041845212fbecf39
Habeetech <oluhabnoni@gmail.com>
2026-05-23 19:59:44 +0100

    added prod  script

---
+++ apps/frontend/src/components/profile/settings/SettingsModal.jsx
@@ -0,0 +1,28 @@
+import { AnimatePresence, motion } from "framer-motion"
+import ModalOverlay from "../../options/ModalOverlay.jsx"
+import CloseModal from "../../buttons/CloseModal.jsx"
+
+export default function SettingsModal({ children, onClose }) {
+    return (
+        <ModalOverlay
+            onClose={onClose}
+            children={
+                <AnimatePresence>
+                    <motion.div
+                        className="settings-modal"
+                        initial={{ scale: 0.8, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        exit={{ scale: 0.8, opacity: 0 }}
+                        transition={{ type: "spring", damping: 20, stiffness: 300 }}
+                    >
+                        <CloseModal
+                        onClose={onClose}
+                        className="close"
+                        />
+                        {children}
+                    </motion.div>
+                </AnimatePresence>
+            }
+        />
+    )
+}
